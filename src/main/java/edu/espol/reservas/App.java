e62e8f1fda884836ad577dc8decd19470a493aea
120800 <120800>
2021-01-12 19:53:15 -0500

    intento

--- src/main/java/edu/espol/reservas/App.java
+++ src/main/java/edu/espol/reservas/App.java
@@ -19,6 +19,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setTitle(" ESPOL");
+        System.out.println("hola");
         stage.setScene(scene);
         stage.show();
     }
