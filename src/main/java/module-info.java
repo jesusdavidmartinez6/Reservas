b7bd36f998e7ae2d863639717048da38b67559ae
120800 <120800>
2021-01-15 09:58:42 -0500

    creacion de fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.espol.reservas {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens edu.espol.reservas to javafx.fxml;
     exports edu.espol.reservas;
