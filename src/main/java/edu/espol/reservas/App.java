97eff5c373d1e9f507cafbf6bb3007d29217cfe0
120800 <120800>
2021-01-15 17:16:23 -0500

    pantalla de consulta hoteles

--- src/main/java/edu/espol/reservas/App.java
+++ src/main/java/edu/espol/reservas/App.java
@@ -17,8 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setTitle(" ESPOL");
+        scene = new Scene(loadFXML("Hoteles"), 640, 480);
+        stage.setTitle("Consulta Hotel");
         stage.setScene(scene);
         stage.show();
     }
