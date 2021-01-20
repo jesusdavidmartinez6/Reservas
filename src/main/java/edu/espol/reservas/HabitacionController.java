5fbe543e21294fecd259ac61e01618858d926632
jesusdavidmartinez <jdmartin@espol.edu.ec>
2021-01-19 23:58:37 -0500

    Avance5

--- src/main/java/edu/espol/reservas/HabitacionController.java
+++ src/main/java/edu/espol/reservas/HabitacionController.java
@@ -6,6 +6,10 @@
 package edu.espol.reservas;
 
 import edu.espol.modelos.Habitacion;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -37,6 +41,8 @@ public class HabitacionController implements Initializable {
     private VBox panel1;
     @FXML
     private VBox panel2;
+    @FXML
+    private HBox HboxMensaje;
 
     /**
      * Initializes the controller class.
@@ -50,17 +56,32 @@ public class HabitacionController implements Initializable {
         panelHabitacion.getChildren().clear();
         panel1.getChildren().clear();
         panel2.getChildren().clear();
+        HboxMensaje.getChildren().clear();
         Boolean existe=false;
         for (Habitacion p: Habitacion.habitaciones){
             if (p.getNumeroHabitacion().equalsIgnoreCase(txtnumeroHabitacion.getText())&& p.getNombreHotel().equals("Sonesta")){
                 Label lblmensaje=new  Label("El numero de habitacion existe");
-                panelHabitacion.getChildren().addAll(lblmensaje);
+                TextField nombre1=new TextField(p.getNombreHotel());
+                TextField numero1=new TextField(p.getNumeroHabitacion());
+                TextField precio1=new TextField(p.getPrecio());
+                TextField servicios1=new TextField(p.getServicio());
+                TextField categoria1=new TextField(p.getCategoria());
+                Label nombre=new  Label("Nombre del Hotel");
+                Label numero=new  Label("Numero del Hotel");
+                Label precio=new  Label("Precio");
+                Label servicios=new  Label("Servicio");
+                Label categoria=new Label("Categoria");
+                HboxMensaje.getChildren().add(lblmensaje);
+                panel2.getChildren().addAll(nombre1,numero1,precio1,servicios1,categoria1);
+                panel1.getChildren().addAll(nombre,numero,precio,servicios,categoria);
+                panelHabitacion.getChildren().addAll(panel1,panel2);
                 existe=true;
             }
       
         }
         if (existe==false){  
-        
+            Label lblmensaje=new  Label("LA HABITACION NO EXISTE."); 
+            HboxMensaje.getChildren().add(lblmensaje);
             TextField precio1=new TextField();
             TextField servicios1=new TextField();
             ComboBox categoria1=new ComboBox();
@@ -68,11 +89,51 @@ public class HabitacionController implements Initializable {
             Label precio=new  Label("Precio");
             Label servicios=new  Label("Servicio");
             Label categoria=new Label("Categoria");
-            panel2.getChildren().addAll(precio1,servicios1,categoria1);
+            Button btnGuardar=new Button("Guardar Información                                                                                                                                                                                ");
+            btnGuardar.setOnAction(e->guardar(txtnumeroHabitacion,precio1,servicios1,categoria1));
+            panel2.getChildren().addAll(precio1,servicios1,categoria1,btnGuardar);
             panel1.getChildren().addAll(precio,servicios,categoria);
             panelHabitacion.getChildren().addAll(panel1,panel2);
-            
+        }
+    }
+    public void guardar(TextField txtnumeroHabitacion,TextField precio, TextField servicio,ComboBox categoria){
+        System.out.println("Sonesta");
+        System.out.println(txtnumeroHabitacion.getText());
+        System.out.println(precio.getText());
+        System.out.println(servicio.getText());
+        System.out.println(categoria.getValue());
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        try {
+            String data = "Sonesta;"+txtnumeroHabitacion.getText()+";"+precio.getText()+";"+servicio.getText()+";"+categoria.getValue();
+            File file = new File("src/main/resources/files/Habitacion.txt");
+        // Si el archivo no existe, se crea!
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        // flag true, indica adjuntar información al archivo.
+        fw = new FileWriter(file.getAbsoluteFile(), true);
+        bw = new BufferedWriter(fw);
+        bw.write(data);
+        bw.newLine();
+        System.out.println("información agregada!");
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        } 
+        finally {
+            try {//Cierra instancias de FileWriter y BufferedWriter
+                if (bw != null)
+                bw.close();
+                if (fw != null)
+                fw.close();
+            } 
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
     
+
+   
 }
